b8706fb0cec1cb93ebc62ceb0cb50b3a32164df6
Shane Cummings <shanecummings_uk@yahoo.co.uk>
2025-01-22 17:06:33 +0000

    Update assembly informational version and cache inputs

--- obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net9.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f76fcf7907130f5f13f9c72047d89d26c5a7d9b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993f8a873b6f3c6e89601ca867ab28a0bc04bad6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
