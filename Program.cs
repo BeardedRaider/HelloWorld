7ad71d21d34a3e5e2feafcf7b656dd224f809aec
Shane Cummings <shanecummings_uk@yahoo.co.uk>
2025-01-22 12:39:22 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System; // This is a namespace that contains the Console class allows us to write to the console using various methods like variable interpolation, string concatenation, etc.
+
+namespace HelloWorld // This is a namespace that contains the Program class. Namespaces are used to organize code and prevent naming conflicts.
+{
+    class Program // This is a class that contains the Main method. The Main method is the entry point of the program.
+    {
+        static void Main(string[] args) // This is the entry point of the program. The Main method is the first method that gets called when the program is run.
+        {
+            Console.WriteLine("Hello World!"); // This is a method that writes the string "Hello World!" to the console.
+        }
+    }
+}
+
